7826915168597132f2bf998c9a56a9f091cb9405
Simon Wellnhofer <siwelli93@gmail.com>
2024-07-09 15:18:24 +0200

    Add custom domain to ALLOWED_HOSTS

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['simon-wellnhofer.de']
+ALLOWED_HOSTS = ['simon-wellnhofer.de', 'www.simon-wellnhofe.de', 'pointy-hamlet-4sxct1klnturbrih8c5rwjgm.herokudns.com']
 
 
 # Application definition
