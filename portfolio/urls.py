0d86b0900c4e121d7c099093d72809f405f9ea5d
Simon Wellnhofer <siwelli93@gmail.com>
2024-07-09 11:21:03 +0200

    create superuser

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,6 +8,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('privacy/', views.privacy, name='privacy'),
     path('terms/', views.terms, name='terms'),
+    path('admin/', admin.site.urls),
     # Project Links
     path('projects/', views.projects, name='projects'),
     path('projects/gda_calculator/', views.gda_calculator, name='gda_calculator'),
