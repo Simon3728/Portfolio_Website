6ff4dfeb29f71bd426656e341d5aa3ebe33055f7
Simon Wellnhofer <siwelli93@gmail.com>
2024-07-09 08:42:15 +0200

    settings.py

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio',
-    'us_election',
+    'myportfolio.portfolio',
+    'myportfolio.us_election',
+    'myportfolio.connect4',
 ]
 
 MIDDLEWARE = [
