f37f70b7e2bb2574a91733f407f8b97e1ae9cae4
Simon Wellnhofer <siwelli93@gmail.com>
2024-07-09 10:42:26 +0200

    Fix123

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'portfolio',
     'us_election',
@@ -125,14 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
 
