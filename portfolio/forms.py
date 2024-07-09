c98185ad8b55747ca0ea21fe39e615f20ad2605b
Simon Wellnhofer <siwelli93@gmail.com>
2024-07-09 12:20:02 +0200

    Configured GMX Mail SMTP for email sending

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 # Form for contact
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    subject = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control text-black'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control text-black'}))
+    subject = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control text-black'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control text-black'}))
