69f43e5a8371a1d1d1d68f2992fe94fef0112a8f
Simon Wellnhofer <siwelli93@gmail.com>
2024-07-09 15:29:18 +0200

    Add custom domain to ALLOWED_HOSTS

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['simon-wellnhofer.de', 'www.simon-wellnhofe.de', 'pointy-hamlet-4sxct1klnturbrih8c5rwjgm.herokudns.com']
+ALLOWED_HOSTS = ['simon-wellnhofer.de', 'www.simon-wellnhofe.de', 'simon-wellnhofer-portfolio-49b0377aeefd.herokuapp.com']
 
 
 # Application definition
